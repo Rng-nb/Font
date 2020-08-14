fa9f7cbeae98197cf2c52adfabfc8c36cdc0ca21
Rng-nb <18782824515@163.com>
2020-08-14 15:20:43 +0800

    [yafeng Wang] add bussiness info

--- src/app/component/Bussiness/Bussiness.js
+++ src/app/component/Bussiness/Bussiness.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import picture from "../../../images/keke.JPG";
+import picture from "../../../images/kele.JPG";
+import 'bootstrap/dist/css/bootstrap.css';
+import "./Bussiness.css";
 class Bussiness extends React.Component {
     render() {
         return (
-        <div>
-          <div>
+        <div className="bussinessinfo">
+          <div className="bussinesspic">
               <img src={picture}/>
+          </div>
+          <div className="bussinessdes">
+              <div>可乐1</div>
+              <div>单价：1元/瓶</div>
+              <span class="badge badge-pill badge-primary">+</span>
           </div>
         </div>);
     }
--- src/app/component/Home/Home.js
+++ src/app/component/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Bussiness from '../Bussiness/Bussiness';
 import './home.css';
 const Home = () => {
   return (
     <div>
       <div className="homebody">
-        Home
+        <Bussiness />
       </div>
       <div className="homefoot">
       TW Mall 2018 Created By ForCheng
